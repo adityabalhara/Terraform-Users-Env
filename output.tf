9e44a4759bc18eb3b828d42f3a686b323800664e
Aditya Balhara <aditya.balhara@gmail.com>
2020-03-08 23:14:26 +0000

    Initial Commit

---
+++ output.tf
@@ -0,0 +1,7 @@
+#Output to a single line
+output "Users" {
+  value = {
+    for key, value in aws_iam_access_key.user:
+    key => { username: value.user, id: value.id, secret: value.secret }
+  }
+}
